305787461739ef05554c6928d33ca707918d0b02
Moises Home <moisesalvesboreges@gmai.com>
2019-12-02 14:45:26 -0200

    Trabalhando com aluno reprovado

--- select/turma/carregaTable_matriculados.php
+++ select/turma/carregaTable_matriculados.php
@@ -6,9 +6,17 @@ $sql = "select * from matricula, aluno where matricula.aluno = aluno.id_aluno an
 $result=mysqli_query($link, $sql);
 if(mysqli_affected_rows($link)>0){
     while($row= mysqli_fetch_assoc($result)){        
+        if($row['situacao']==1){
+            $situacao="Matriculado";
+        }else if($row['situacao']==2){
+            $situacao = "Aprovado";
+        }else if($row['situacao']==3){
+            $situacao = "Reprovado"
+        }
         echo"<tr>"
         . "<td>".$row['numero']."</td>"
         . "<td>".utf8_encode($row['nome_aluno'])."</td>"        
+        . "<td>".$situacao."</td>"        
         . "<td class=\"text-center\">"
             . "<button  class='btn btn-success btn-aprovado' data-nome='".utf8_encode($row['nome_aluno'])."' data-cod='".$row['numero']."'>Aprovado</button>&nbsp;&nbsp;"
             . "<button  class='btn btn-warning btn-reprovado' data-nome='".utf8_encode($row['nome_aluno'])."' data-cod='".$row['numero']."'>Reprovado</i></button>&nbsp;&nbsp;"
